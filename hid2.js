fa0d688518f2580b9c069c47d0ad343badc76174
dio <32084600+nazzetta@users.noreply.github.com>
2023-05-15 18:23:02 -0300

    Fixed the inverted number

--- hid2.js
+++ hid2.js
@@ -1,2 +1,3 @@
+// in here, the rest of the number is inverted, for safety
 // var testlink1 = '461827';   // american #
-var testlink1 = '1118685';  // brazilian #
+var testlink1 = '5868111';  // brazilian #
